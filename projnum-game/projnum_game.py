ad828d5bf615c1520443bc2f1ecac287d8cc62d6
yann1822 <yann.simonot0022@gmail.com>
2026-03-31 19:36:27 +0200

    Add files via upload
    - Ajout d'une couleur en fonction du type de neutron (blanc = neutrons lents et violet = neutrons rapides)
    
    - J'avais oublié d'intégrer les méthodes numpy pour la températures moyenne et la quantité de vapeur
    
    - Ajout d'un contrôle du temps avec les flèches haut et bas pour pouvoir mieux observer les effets long terme comme le refroidissement par conduction

--- projnum-game/projnum_game.py
+++ projnum-game/projnum_game.py
@@ -11,9 +11,10 @@ fps = 60
 
 noir = (10, 10, 10)
 blanc = (245, 245, 245)
+violet = (148, 0, 211)
 
 bleu = (50, 100, 255)
-jaune = (255, 100, 50)
+jaune = (255, 255, 0)
 orange = (255, 165, 0)
 rouge = (255, 50, 50)
 
@@ -29,6 +30,9 @@ class RightMenu:
     temp_rect: pygame.Rect;
     nbNeutron_rect: pygame.Rect;
     vapQuantity_rect: pygame.Rect;
+    speed_surface: pygame.Surface
+    speed_rect: pygame.Rect
+    sim_speed_val: int = 1
 
     temp: float;
     nbNeutron: int;
@@ -47,14 +51,19 @@ class RightMenu:
         posY += self.spacing
         self.vapQuantity_surface = self.font.render("Quantité de vapeur :", True, (255, 255, 255))
         self.vapQuantity_rect = self.vapQuantity_surface.get_rect(topleft=(610, posY))
+        posY += self.spacing
+        self.speed_surface = self.font.render("Vitesse simulation :", True, (255, 255, 255))
+        self.speed_rect = self.speed_surface.get_rect(topleft=(610, posY))
         self.temp = 0;
         self.nbNeutron = 0;
         self.vapQuantity = 0;
+        self.sim_speed_val = 1
 
     def display_menu(self, screen):
         screen.blit(self.temp_surface, self.temp_rect)
         screen.blit(self.nbNeutron_surface, self.nbNeutron_rect)
         screen.blit(self.vapQuantity_surface, self.vapQuantity_rect)
+        screen.blit(self.speed_surface, self.speed_rect)
         
         posY = self.start; #pix
         temp_surface = self.font.render(f"{self.temp:.0f} °K", True, (255, 255, 255))
@@ -68,14 +77,19 @@ class RightMenu:
         vapQuantity_surface = self.font.render(f"{self.vapQuantity}", True, (255, 255, 255))
         vapQuantity_rect = vapQuantity_surface.get_rect(topright=(790, posY))
         screen.blit(vapQuantity_surface, vapQuantity_rect)
+        posY += self.spacing
+        speed_val_surface = self.font.render(f"{self.sim_speed_val}", True, (255, 255, 255))
+        speed_val_rect = speed_val_surface.get_rect(topright=(790, posY))
+        screen.blit(speed_val_surface, speed_val_rect)
 
-    def computeMetrics(self, neutrons, grid):
-        self.nbNeutron = len(neutrons);
-        self.temp = sum([grid[i, j, 0] for i in range(cols) for j in range(rows)])/(cols*rows);
-        self.vapQuantity = sum([1 for i in range(cols) for j in range(rows) if grid[i, j, 0] >= T_ev]);
+    def computeMetrics(self, neutrons, grid, current_speed):
+        self.nbNeutron = len(neutrons)
+        self.temp = np.mean(grid[:, :, 0])
+        self.vapQuantity = np.sum(grid[:, :, 0] >= T_ev)
+        self.sim_speed_val = current_speed
 
-rightMenu = RightMenu();
-rightMenu.prepare_menu();
+rightMenu = RightMenu()
+rightMenu.prepare_menu()
 
 pxTom = 10e-2/20 #Facteur de conversion en m/px (permet de passer de px à m, ici 1px = 0.05cm =5e-4m)
 CToK = 273.15 #Conversion Celsius-Kelvin
@@ -95,13 +109,14 @@ nbr_nav = 5 #Nombre de neutrons intéragissant avant évaporation --> pour déte
 q_ad_slow = m_eau*C_me*(T_ev-T0)/(Ec_slow*nbr_nav) #Facteur d'adaptation pour la simu --> permet de chauffer plus vite par les neutrons thermiques
 q_ad_fast = m_eau*C_me*(T_ev-T0)/(Ec_fast*nbr_nav) #Facteur d'adaptation pour la simu --> permet de chauffer plus vite par les neutrons thermiques
 
-p_int_rapide = 15 #Probabilité d'intéraction des neutrons rapides avec l'eau en %
+p_int_rapide = 10 #Probabilité d'intéraction des neutrons rapides avec l'eau en %
 p_abs_lente = 0.1*p_int_rapide #Probabilité d'absorption des neutrons lents en %
 p_n0_rapides = 50 #Proportion de neutrons rapides à l'apparition en %
 
-Palier1 = T0+(T_ev-T0)/4 #Premier palier de température ici de 20°C à 40°C
-Palier2 = T0+2*(T_ev-T0)/4 #Second palier donc de 40°C à 60°C
-Palier3 = T0+3*(T_ev-T0)/4 #Second palier donc de 40°C à 60°C
+Interval = (T_ev-T0)/4 #Étendue divisé par le nbr d'intervales souhaités
+Palier1 = T0+Interval #Premier palier de température ici de 20°C à 40°C
+Palier2 = T0+2*Interval #Second palier donc de 40°C à 60°C
+Palier3 = T0+3*Interval #Second palier donc de 40°C à 60°C
 
 delta_t = 1/fps #Écart temporel d'une itération à l'autre (en s)
 k1 = 2.4e-3*Gamma #Constante pour la loi de Newton - conducto convectif à l'interface supérieure
@@ -125,86 +140,95 @@ class Neutron:
         self.vy = self.v * np.sin(self.angle)
         self.isFast = (self.v == 3)
         self.v_real = (2*Ec_fast/m_n)**0.5 if self.isFast else (2*Ec_slow/m_n)**0.5
+        self.color = violet if self.isFast else blanc
         
     def deplacer(self):
         self.x += self.vx
         self.y += self.vy
 
     def dessiner(self, surface):
-        pygame.draw.circle(surface, blanc, (int(self.x), int(self.y)), self.taille)
+        pygame.draw.circle(surface, self.color, (int(self.x), int(self.y)), self.taille)
 
 neutrons = []
 running = True
+sim_speed = 1
 
 while running:
     screen.fill(noir)
-    rightMenu.computeMetrics(neutrons, grid)
+    rightMenu.computeMetrics(neutrons, grid, sim_speed)
     rightMenu.display_menu(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                sim_speed += 1
+            elif event.key == pygame.K_DOWN:
+                sim_speed = max(1, sim_speed-1)
 
     if pygame.mouse.get_pressed()[0]:
         mouse_x, mouse_y = pygame.mouse.get_pos()
         neutrons.append(Neutron(mouse_x, mouse_y)) #On ajoute les neutrons en dessous du curseur
 
-    for n in neutrons[:]:
-        n.deplacer()
+    for _ in range(sim_speed):
+        for n in neutrons[:]:
+            n.deplacer()
 
-        if n.x < 0 or n.x > width - rightMenuSize or n.y < 0 or n.y > height: #Si le neutron sort de l'écran on le supprime
-            neutrons.remove(n)
-            continue
+            if n.x < 0 or n.x > width - rightMenuSize or n.y < 0 or n.y > height: #Si le neutron sort de l'écran on le supprime
+                neutrons.remove(n)
+                continue
 
-        grid_x, grid_y = int(n.x//cell_size), int(n.y//cell_size) #Coordonnées du neutron cible dans la base des cases
+            grid_x, grid_y = int(n.x//cell_size), int(n.y//cell_size) #Coordonnées du neutron cible dans la base des cases
 
-        if 0 <= grid_x < cols and 0 <= grid_y < rows: #On verifie que le neutron soit bien dans l'écran
-            if grid[grid_x, grid_y, 0] <= T_ev: #Si la case contient de l'eau liquide
-                interact_rapide = random.choices([0, 1], weights=[100-p_int_rapide, p_int_rapide])[0] #On lance les dés pour l'intéraction rapide
-                absorption_lent = random.choices([0, 1], weights=[100-p_abs_lente, p_abs_lente])[0] #Idem pour l'absorption lente
+            if 0 <= grid_x < cols and 0 <= grid_y < rows: #On verifie que le neutron soit bien dans l'écran
+                if grid[grid_x, grid_y, 0] <= T_ev: #Si la case contient de l'eau liquide
+                    interact_rapide = random.choices([0, 1], weights=[100-p_int_rapide, p_int_rapide])[0] #On lance les dés pour l'intéraction rapide
+                    absorption_lent = random.choices([0, 1], weights=[100-p_abs_lente, p_abs_lente])[0] #Idem pour l'absorption lente
 
-                if n.isFast and interact_rapide == 1:
-                    grid[grid_x, grid_y, 0] += q_ad_fast*(Ec_fast-Ec_slow)/(m_eau*C_me) #Chaleur fournie par le neutron rapide
-                    n.v = 1 #Ralentissement du neutron rapide
-                    n.actu_vitesse()
-                elif not n.isFast and absorption_lent == 1:
-                    grid[grid_x, grid_y, 0] += q_ad_slow*Ec_slow/(m_eau*C_me) #Chaleur fournie par le neutron lent (concrètement négligeable)
-                    if n in neutrons:
-                        neutrons.remove(n) #Le neutron lent est quant à lui absorbé donc il disparait
-                        continue
+                    if n.isFast and interact_rapide == 1:
+                        grid[grid_x, grid_y, 0] += q_ad_fast*(Ec_fast-Ec_slow)/(m_eau*C_me) #Chaleur fournie par le neutron rapide
+                        n.v = 1 #Ralentissement du neutron rapide
+                        n.actu_vitesse()
+                    elif not n.isFast and absorption_lent == 1:
+                        grid[grid_x, grid_y, 0] += q_ad_slow*Ec_slow/(m_eau*C_me) #Chaleur fournie par le neutron lent (concrètement négligeable)
+                        if n in neutrons:
+                            neutrons.remove(n) #Le neutron lent est quant à lui absorbé donc il disparait
+                            continue
 
-    T = grid[:, :, 0] #On isole la matrice des températures
-    diff = np.zeros_like(T) #On génère une matrice qui contiendra les flux
+        T = grid[:, :, 0] #On isole la matrice des températures
+        diff = np.zeros_like(T) #On génère une matrice qui contiendra les flux
 
-    diff[:-1, :] += T[1:, :] - T[:-1, :] #Flux vertical du dessous
-    diff[1:, :] += T[:-1, :] - T[1:, :] #Flux vertical du dessus
-    diff[:, :-1] += T[:, 1:] - T[:, :-1] #Flux horizontal de la droite
-    diff[:, 1:] += T[:, :-1] - T[:, 1:] #Flux horizontal de la gauche
+        diff[:-1, :] += T[1:, :] - T[:-1, :] #Flux vertical du dessous
+        diff[1:, :] += T[:-1, :] - T[1:, :] #Flux vertical du dessus
+        diff[:, :-1] += T[:, 1:] - T[:, :-1] #Flux horizontal de la droite
+        diff[:, 1:] += T[:, :-1] - T[:, 1:] #Flux horizontal de la gauche
 
-    coeff_conduction = min(0.24, k2) #On plafonne à 1/4, la limite de k2 pour 4 voisines
-    isEv = (T<T_ev).astype(float) #Ici on renvoie 0 si c'est de la vapeur et 1 si c'est liquide, car conduction que pour le liquide
+        coeff_conduction = min(0.24, k2) #On plafonne à 1/4, la limite de k2 pour 4 voisines
+        isEv = (T<T_ev).astype(float) #Ici on renvoie 0 si c'est de la vapeur et 1 si c'est liquide, car conduction que pour le liquide
 
-    grid[:, :, 0] += coeff_conduction*diff*isEv #On met à jour
-    
-    # Remontée des bulles de vapeur
-    for i in range(cols):
-        for j in range(rows):
-            #mécanisme de refroidissement continu
-            if (grid[i, j, 0] > T0 and j == 0):
-                Ti = grid[i, j, 0]
-                grid[i, j, 0] = Ti-k1*(Ti-T0)*delta_t #On fait baisser la température de la case progressivement
+        grid[:, :, 0] += coeff_conduction*diff*isEv #On met à jour
+        
+        # Remontée des bulles de vapeur
+        for i in range(cols):
+            for j in range(rows):
+                #mécanisme de refroidissement continu
+                if (grid[i, j, 0] > T0 and j == 0):
+                    Ti = grid[i, j, 0]
+                    grid[i, j, 0] = Ti-k1*(Ti-T0)*delta_t #On fait baisser la température de la case progressivement
 
-            if grid[i, j, 0] >= T_ev: #Si la case contient de la vapeur
-                if (j > 0 and grid[i, j-1, 0] < T_ev):
-                    grid[i, j, 1] += 1 #On incrémente le compteur
-                    if (grid[i, j, 1] >= 10):
-                        tmp = grid[i, j-1, 0]
-                        grid[i, j-1, 0] = grid[i, j, 0]
-                        grid[i, j, 0] = tmp
-                        grid[i, j-1, 1] = 0
+                if grid[i, j, 0] >= T_ev: #Si la case contient de la vapeur
+                    if (j > 0 and grid[i, j-1, 0] < T_ev):
+                        grid[i, j, 1] += 1 #On incrémente le compteur
+                        if (grid[i, j, 1] >= 10):
+                            tmp = grid[i, j-1, 0]
+                            grid[i, j-1, 0] = grid[i, j, 0]
+                            grid[i, j, 0] = tmp
+                            grid[i, j-1, 1] = 0
+                            grid[i, j, 1] = 0
+                    else:
                         grid[i, j, 1] = 0
-                else:
-                    grid[i, j, 1] = 0
 
     # Affichage des cases d'eau
     for i in range(cols):
